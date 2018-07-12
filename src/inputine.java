9799ea1a999022a543d82f9f5b329eea4b2581cb
Tafkir Ahmed <ahmedtafkir@gmail.com>
2018-07-12 10:00:48 -0400

    still working

---
+++ src/inputine.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class inputine
+{
+    public static void main( String[] args )
+    {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        double num1, num2, num3;
+
+        System.out.print( "First temperature? " );
+        num1 = keyboard.nextDouble();
+
+        System.out.print( "Second temperature? " );
+        num2 = keyboard.nextDouble();
+
+
+        System.out.println("The max value is : " + Math.max(num1, num2));
+    }
+}
